d1a0201facc19158df68edc4005facbb62d912ef
Saki Tojo <haolei.ye@anu.edu.au>
2023-04-06 05:59:41 +1000

    Add some readme doc.

--- hana.py
+++ hana.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 import sys
 import argparse
+import automated.config as config
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--project', help='Project identifier', required=True)
-    parser.add_argument('-f', '--fasta', help='Contig FASTA file (.fasta/.fasta.gz)', required=True)
-    parser.add_argument('-1', '-f', '--hic-forward', nargs='+', help='Forward Hi-C reads (.fastq.gz)', required=True)
-    parser.add_argument('-2', '-r', '--hic-reverse', nargs='+', help='Reverse Hi-C reads (.fastq.gz)', required=True)
-    parser.add_argument('-g', '--groups', help='Group of Chromosome (Integer)', required=True)
+    parser.add_argument('-p', '--project-dir', help='Project directory')
+    parser.add_argument('-f', '--fasta', help='Contig FASTA file (.fasta/.fasta.gz)')
+    parser.add_argument('-1', '-f', '--hic-forward', nargs='+', help='Forward Hi-C reads (.fastq.gz)')
+    parser.add_argument('-2', '-r', '--hic-reverse', nargs='+', help='Reverse Hi-C reads (.fastq.gz)')
+    parser.add_argument('-g', '--groups', help='Group of Chromosome (Integer)')
     parser.add_argument('-s', '--skip', help='Steps to be skipped')
-    parser.add_argument('-e', '--enzyme', help='Restriction Enzyme (default: HindIII)', default='HindIII')
+    parser.add_argument('-e', '--enzyme', help='Restriction Enzyme (default: HindIII)')
     return parser.parse_args()
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,35 @@
 # HANA
 
-High-performance Automated Next-generation AllHiC
+High-performance Automated Next-generation AllHiC.
+
+## Dependencies
+
+- MSVC / GCC with C++ 11 support
+- CMake
+- Python 3.10
+
+## Usage
+
+### Arguments mode
+
+```bash
+hana -p PROJECT_DIR -f FASTA_PATH -1 [FORWARD_PAIRS] -2 [REVERSE_PAIRS] -g 12
+```
+Arguments explanation:
+- `-p` or `--project-dir`: specify the project working directory.
+- `-f` or `--fasta`: the config FASTA file. Its suffix should be either `.fasta` or `.fasta.gz`.
+- `-1` or `--hic-forward`: the forward Hi-C FASTQ file. Its suffix should be `.fastq.gz`.
+- `-2` or `--hic-reverse`: the reverse Hi-C FASTQ file. Its suffix should be `.fastq.gz`.
+- `-g` or `--groups`: the number of chromosome groups.
+
+Optional arguments:
+
+- `-e` or `--enzyme`: the restriction sites used for the data. Can be a specific name or a small sequence. For more information, please check [HANA supported restriction sites](## Supported restriction sites). Default value is `HindIII`.
+```bash
+hana /path/to/directory
+```
+
+### Configuration file mode
+
+## Supported restriction sites
+
